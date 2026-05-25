4357f5ee96b2ff3c4f449c3c76c5664f160d5066
jennifer <jenny.hu615@gmail.com>
2026-05-25 08:44:44 -0700

    frontend fully functional

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:5001";
+const API_URL = "http://localhost:3000";
 
 function App() {
   const [posts, setPosts] = useState([]);
